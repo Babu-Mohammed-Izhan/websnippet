024af1f8fbfb2c4e8f1f36c47c875a22a6fe35de
Izhan <babuizhan1234@gmail.com>
2021-10-16 14:11:54 +0530

    fixed columns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, ChakraProvider, SimpleGrid } from "@chakra-ui/react";
 import Navigation from "./component/Navigation";
 import Codecard from "./component/Codecard";
 import "./App.css";
 
 function App() {
+  // useEffect(() => {
+  //   axios.get(``, (res) => {
+  //     cosole.log(res.data);
+  //   });
+  // }, []);
+
   return (
     <ChakraProvider>
       <Box bgColor="purple.800">
         <Navigation />
         <Box bgColor="purple.800" minH="100vh">
           <Box marginLeft="15px" marginRight="15px" marginTop="20px">
-            <SimpleGrid columns={[1, 2, 3, 4]} spacing="20px">
+            <SimpleGrid columns={[1, null, 2, 3]} spacing="20px">
               <Codecard
                 language="javascript"
                 code="console.log('Hello');"
