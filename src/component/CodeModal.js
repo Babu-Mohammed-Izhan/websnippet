f58eff70bf46976ce9c31d6d5175de07931914ef
Izhan <babuizhan1234@gmail.com>
2021-10-16 22:18:53 +0530

    added code editor and formik support

--- src/component/CodeModal.js
+++ src/component/CodeModal.js
@@ -10,15 +10,28 @@ import {
   Button,
   FormControl,
   FormLabel,
-  FormErrorMessage,
   Input,
 } from "@chakra-ui/react";
 
 import CodeEditor from "@uiw/react-textarea-code-editor";
-import { Form, Formik } from "formik";
+import { useFormik } from "formik";
 
 const CodeModal = ({ isOpen, onClose }) => {
   const [code, setCode] = useState("");
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      language: "",
+      author: "",
+    },
+    onSubmit: (values) => {
+      console.log(values, code);
+      formik.resetForm();
+      setCode("");
+    },
+  });
+
   return (
     <>
       <Modal isOpen={isOpen} size="xl" onClose={onClose} colorScheme="purple">
@@ -27,66 +40,58 @@ const CodeModal = ({ isOpen, onClose }) => {
           <ModalHeader size="2xl">Modal Title</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Formik
-              onSubmit={(values, actions) => {
-                console.log(values);
-              }}
-            >
-              {(props) => (
-                <Form>
-                  {({ field, form }) => (
-                    <FormControl id="title" isRequired>
-                      <FormLabel>Title</FormLabel>
-                      <Input {...field} placeholder="Title" />
-                      <FormErrorMessage>{form.errors.title}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                  {({ field, form }) => (
-                    <FormControl id="language" isRequired>
-                      <FormLabel>Language</FormLabel>
-                      <Input {...field} placeholder="Language" />
-                      <FormErrorMessage>
-                        {form.errors.language}
-                      </FormErrorMessage>
-                    </FormControl>
-                  )}
-                  {({ field, form }) => (
-                    <FormControl id="author" isRequired>
-                      <FormLabel>Author</FormLabel>
-                      <Input {...field} placeholder="Author" />
-                      <FormErrorMessage>{form.errors.author}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                  {({ field, form }) => (
-                    <FormControl id="code" isRequired>
-                      <FormLabel>Code</FormLabel>
-                      <CodeEditor
-                        {...field}
-                        value={code}
-                        language="js"
-                        placeholder="Please enter JS code."
-                        onChange={(e) => setCode(e.target.value)}
-                        padding={15}
-                        style={{
-                          fontSize: 12,
-                          backgroundColor: "#f5f5f5",
-                          fontFamily:
-                            "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-                        }}
-                      />
-                    </FormControl>
-                  )}
-                  <Button
-                    colorScheme="purple"
-                    mr={3}
-                    onClick={onClose}
-                    isLoading={props.isSubmitting}
-                  >
-                    Submit
-                  </Button>
-                </Form>
-              )}
-            </Formik>
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl id="title" isRequired>
+                <FormLabel>Title</FormLabel>
+                <Input
+                  placeholder="Title"
+                  onChange={formik.handleChange}
+                  value={formik.values.title}
+                />
+              </FormControl>
+
+              <FormControl id="language" isRequired>
+                <FormLabel>Language</FormLabel>
+                <Input
+                  placeholder="Language"
+                  onChange={formik.handleChange}
+                  value={formik.values.language}
+                />
+              </FormControl>
+
+              <FormControl id="author" isRequired>
+                <FormLabel>Author</FormLabel>
+                <Input
+                  placeholder="Author"
+                  onChange={formik.handleChange}
+                  value={formik.values.author}
+                />
+              </FormControl>
+              <FormControl id="code" isRequired>
+                <FormLabel>Code</FormLabel>
+                <CodeEditor
+                  value={code}
+                  language="js"
+                  placeholder="Please enter JS code."
+                  onChange={(e) => setCode(e.target.value)}
+                  padding={15}
+                  style={{
+                    fontSize: 12,
+                    backgroundColor: "#f5f5f5",
+                    fontFamily:
+                      "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                  }}
+                />
+              </FormControl>
+              <Button
+                colorScheme="purple"
+                mr={3}
+                onClick={onClose}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </form>
           </ModalBody>
 
           <ModalFooter>
