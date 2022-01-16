854fcce6a0af4acb43ae516546b4964a3661357e
Izhan <babuizhan1234@gmail.com>
2022-01-16 21:02:18 +0530

    changed url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   Container,
   Stack,
@@ -13,16 +13,16 @@ import {
   SimpleGrid,
   useDisclosure,
   Spinner,
-} from "@chakra-ui/react";
-import Navigation from "./component/Navigation";
-import Codecard from "./component/Codecard";
-import CodeModal from "./component/CodeModal";
-import "./App.css";
+} from '@chakra-ui/react';
+import Navigation from './component/Navigation';
+import Codecard from './component/Codecard';
+import CodeModal from './component/CodeModal';
+import './App.css';
 
 export const Blob = (props) => {
   return (
     <Icon
-      width={"100%"}
+      width={'100%'}
       viewBox="0 0 578 440"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +42,7 @@ function App() {
   const [data, setdata] = useState([]);
   const [filteredData, setfilteredData] = useState(data);
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/api/snip`)
+    fetch(`${process.env.REACT_APP_API_URL}api/snip`)
       .then((res) => res.json())
       .then((data) => {
         setdata(data);
@@ -72,41 +72,41 @@ function App() {
           handleOpenModal={() => openEnterModal()}
           handleSearch={handleSearch}
         />
-        <Container maxW={"7xl"}>
+        <Container maxW={'7xl'}>
           <Stack
-            align={"center"}
+            align={'center'}
             spacing={{ base: 8, md: 10 }}
             py={{ base: 20, md: 28 }}
-            direction={{ base: "column", md: "row" }}
+            direction={{ base: 'column', md: 'row' }}
           >
             <Stack flex={1} spacing={{ base: 5, md: 10 }}>
               <Heading
                 lineHeight={1.1}
                 fontWeight={600}
-                fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+                fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
               >
                 <Text
-                  as={"span"}
-                  position={"relative"}
+                  as={'span'}
+                  position={'relative'}
                   _after={{
                     content: "''",
-                    width: "full",
-                    height: "30%",
-                    position: "absolute",
+                    width: 'full',
+                    height: '30%',
+                    position: 'absolute',
                     bottom: 1,
                     left: 0,
-                    bg: "purple.400",
+                    bg: 'purple.400',
                     zIndex: -1,
                   }}
                 >
                   Write once,
                 </Text>
                 <br />
-                <Text as={"span"} color={"purple.500"}>
+                <Text as={'span'} color={'purple.500'}>
                   use everywhere!
                 </Text>
               </Heading>
-              <Text color={"gray.500"}>
+              <Text color={'gray.500'}>
                 Snip is a rich coding snippets app that lets you create your own
                 code snippets, use other popular snippets and copy them to your
                 clipboard with a single click. All that is free!
@@ -114,36 +114,36 @@ function App() {
             </Stack>
             <Flex
               flex={1}
-              justify={"center"}
-              align={"center"}
-              position={"relative"}
-              w={"full"}
+              justify={'center'}
+              align={'center'}
+              position={'relative'}
+              w={'full'}
             >
               <Blob
-                w={"150%"}
-                h={"150%"}
-                position={"absolute"}
-                top={"-20%"}
+                w={'150%'}
+                h={'150%'}
+                position={'absolute'}
+                top={'-20%'}
                 left={0}
                 zIndex={-1}
-                color={useColorModeValue("purple.50", "purple.400")}
+                color={useColorModeValue('purple.50', 'purple.400')}
               />
               <Box
-                position={"relative"}
-                height={"300px"}
-                rounded={"2xl"}
-                boxShadow={"2xl"}
-                width={"full"}
-                overflow={"hidden"}
+                position={'relative'}
+                height={'300px'}
+                rounded={'2xl'}
+                boxShadow={'2xl'}
+                width={'full'}
+                overflow={'hidden'}
               >
                 <Image
-                  alt={"Hero Image"}
-                  fit={"cover"}
-                  align={"center"}
-                  w={"100%"}
-                  h={"100%"}
+                  alt={'Hero Image'}
+                  fit={'cover'}
+                  align={'center'}
+                  w={'100%'}
+                  h={'100%'}
                   src={
-                    "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+                    'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
                   }
                 />
               </Box>
